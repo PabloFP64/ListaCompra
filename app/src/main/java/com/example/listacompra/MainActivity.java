546fc11772ac6645c7bf375be91b5be328acdf35
PabloFP64 <PabloFP64@gmail.com>
2022-11-05 12:53:20 +0100

    La lie y no hice commit hasta aqui casi la lio :/

---
+++ app/src/main/java/com/example/listacompra/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.listacompra;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.listacompra.task_database.TaskContract;
+import com.example.listacompra.task_database.TaskDatabaseHelper;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private TaskDatabaseHelper taskHelper;
+    private ListView TaskList;
+    private ArrayAdapter<String> arrAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        taskHelper = new TaskDatabaseHelper(this);
+        TaskList = (ListView) findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.title_task);
+        String task = String.valueOf(taskTextView.getText());
+        SQLiteDatabase db = taskHelper.getWritableDatabase();
+        db.delete(TaskContract.TaskEntry.TABLE, TaskContract.TaskEntry.COL_TASK_TITLE + " = ?", new String[]{task});
+        db.close();
+        updateUI();
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = taskHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (arrAdapter == null) {
+            arrAdapter = new ArrayAdapter<>(this, R.layout.todo_task, R.id.title_task, taskList);
+            TaskList.setAdapter(arrAdapter);
+        } else {
+            arrAdapter.clear();
+            arrAdapter.addAll(taskList);
+            arrAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+
+    public void crearProducto(View view) {
+        final EditText taskEdit = new EditText(this);
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Add a new task").setMessage("What do you want to do next?").setView(taskEdit)
+                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEdit.getText());
+                        SQLiteDatabase db = taskHelper.getWritableDatabase();
+                        ContentValues values = new ContentValues();
+                        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                        db.insertWithOnConflict(TaskContract.TaskEntry.TABLE, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                        db.close();
+                        updateUI();
+                    }
+                })
+                .setNegativeButton("Cancel", null).create();
+        dialog.show();
+    }
+}
